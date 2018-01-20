4212d1aea4238704db3004fc683255089b1f1196
Lcwben <sizailcw@qq.com>
2018-01-20 21:05:05 +0800

    Test pull.

--- httpClientDemo/src/lcwben/demo1/reqDemo.java
+++ httpClientDemo/src/lcwben/demo1/reqDemo.java
@@ -13,7 +13,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
